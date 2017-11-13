16d60b6a4f1f2f2c01c5142b7d60c3733ce0c57c
Kynan <k.song@blueyonder.co.uk>
2017-11-13 19:03:51 +0000

    Created WordCounter and test classes.

---
+++ app/src/main/java/com/example/kynansong/wordcounter/Models/WordCounter.java
@@ -0,0 +1,8 @@
+package com.example.kynansong.wordcounter.Models;
+
+/**
+ * Created by kynansong on 13/11/2017.
+ */
+
+public class WordCounter {
+}
---
+++ app/src/test/java/com/example/kynansong/wordcounter/WordCounterTest.java
@@ -0,0 +1,21 @@
+package com.example.kynansong.wordcounter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by kynansong on 13/11/2017.
+ */
+
+public class WordCounterTest {
+
+    @Before
+
+
+    @Test
+    public void (){
+        assertEquals();
+    }
+}
