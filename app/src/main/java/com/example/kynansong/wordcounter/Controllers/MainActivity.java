1e715aadc26ea01f23b4539883d00ae4b8bef52c
Kynan <k.song@blueyonder.co.uk>
2017-11-14 19:58:58 +0000

    Added new activities and menu option.

--- app/src/main/java/com/example/kynansong/wordcounter/Controllers/MainActivity.java
+++ app/src/main/java/com/example/kynansong/wordcounter/Controllers/MainActivity.java
@@ -1,14 +1,19 @@
-package com.example.kynansong.wordcounter;
+package com.example.kynansong.wordcounter.Controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.kynansong.wordcounter.Models.WordCounter;
+import com.example.kynansong.wordcounter.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +44,28 @@ public class MainActivity extends AppCompatActivity {
         this.counter.setText(this.wordCounter.wordCount(sentence).toString());
         this.occurences.setText(this.wordCounter.wordOccurences(sentence).toString());
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    //new method per button.
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.action_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+        } else if(item.getItemId() == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
+        return true;
     }
 
 
---
+++ app/src/main/java/com/example/kynansong/wordcounter/Controllers/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.kynansong.wordcounter.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kynansong.wordcounter.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
